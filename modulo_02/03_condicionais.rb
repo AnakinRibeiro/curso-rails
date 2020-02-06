9c8116d83c57b31a8e4e12e851033180ea171658
Anakin Ribeiro <eduardor.contato@outlook.com>
2020-02-06 16:28:38 -0300

    Conceitos básicos de Ruby até Arrays/Vetores

---
+++ modulo_02/03_condicionais.rb
@@ -0,0 +1,43 @@
+# ESTRUTURA CONDICIONAL TERNARIA
+sexo = 'M'
+
+sexo == 'M' ? (puts 'Masculino') : (puts'Feminino')
+
+# CASE
+
+print "Digite uma idade"
+idade = gets.chomp.to_i
+
+case idade
+when 0..2
+    puts "bebe"
+when 3..12          
+    puts "criança"
+when 13..18
+    puts "adolescente"
+else    
+    puts "adulto"
+end
+
+
+# UNLESS
+
+print 'Digite um numero:'
+x = gets.chomp.to_i
+
+unless x >= 2
+    puts "x é menor que 2"
+else
+    puts "x é maior que 2"
+end
+
+# IF
+
+print 'Digite um numero'
+x = gets.chomp.to_i
+
+if x > 2
+    puts 'X é maior que 2'
+end
+
+
